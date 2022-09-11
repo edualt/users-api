7b162b595ad5f5e5b9a1d19d4d4917520e0b8dce
edualt <eduardosebastianhdz503@gmail.com>
2022-09-10 21:36:55 -0500

    "feat(CreateUserRequest): add validations"

--- src/main/java/com/api/users/controllers/dtos/CreateUserRequest.java
+++ src/main/java/com/api/users/controllers/dtos/CreateUserRequest.java
@@ -1,9 +1,20 @@
 package com.api.users.controllers.dtos;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 public class CreateUserRequest {
 
+    @NotNull(message = "username cannot be null")
+    @NotBlank(message = "username cannot be blank")
     private String username;
+    @NotEmpty(message = "The email address is required.")
+    @Email(message = "The email address is invalid.")
     private String email;
+    @NotNull(message = "username cannot be null")
+    @NotBlank(message = "username cannot be blank")
     private String password;
 
 
